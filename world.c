e15b0c3a4ca510874d87b8baa5eb480f77c98248
Owain Cleaver <owain@lor3.org>
2012-05-08 17:59:18 +0100

    Chopper keys are 'released' when console gains focus.

--- world.c
+++ world.c
@@ -240,6 +240,10 @@ static void keypress(void *priv, int key, int down)
 {
 	struct _world *world = priv;
 	switch(key) {
+	case SDLK_UNKNOWN:
+		/* console enabled */
+		chopper_control_release_all(world->apache);
+		break;
 	case SDLK_LEFT:
 		chopper_control(world->apache, CHOPPER_LEFT, down);
 		break;
